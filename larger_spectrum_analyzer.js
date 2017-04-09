15b5ba7cec3017592275626db6cb130bfb659568
Paul Joey Clark <joeytwiddle@gmail.com>
2017-04-09 14:07:11 +0800

    Add larger_spectrum_analyzer.js

---
+++ larger_spectrum_analyzer.js
@@ -0,0 +1,4 @@
+// Larger
+c.width=2e3;for(i=9;i--;)for(j=0;j<80*(.7+S(i+12*t)*C(i+t))*i;j+=150)x.fillStyle=R(j/2,600-j/2,0,1),x.fillRect(1700-i*175,900-j/2,130,50)
+// Faster
+(.8+S(i+14*t)*C(i+t))
