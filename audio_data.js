7cbddf0ae654c573e1007c91472bfa51ac9b372c
Paul Joey Clark <joeytwiddle@gmail.com>
2018-04-06 00:15:49 +0800

    Add audio_data.js

---
+++ audio_data.js
@@ -0,0 +1,36 @@
+h=d='data:audio/wav;base64,'+
+    'UklGRiQAAABXQVZFZm10IBAAAAABAAEAwF0AAMBdAAABAAgAZGF0YQAA';
+for(=0;t++<8e5;)
+  d+=String.fromCharCode(/* ByteBeat */);
+
+// a hi-hat
+var inst = Math.random() - .5;
+int *= Math.pow(1 - (T % 1), 8);
+d += String.fromCharCode(inst * volume + 127);
+
+Z=new Audio(h+btoa(d));
+Z.play();
+
+
+// Instead of HSL
+ctx.fillStyle = 'rgb(' + [
+  127 + 127 * Math.cos(hue),
+  127 + 127 * Math.cos(hue + 2),
+  127 + 127 * Math.cos(hue + 4)
+] + ')';
+
+// @ 9:51
+
+// Someone else also had a theramin idea: https://www.dwitter.net/d/5924
+
+// Try this some time:
+S=900,Q=function(s){return 16&t*Math.pow(2,(1+s.charCodeAt((t/S)%s.length))/12-7)},M=function(s){return Q(s)+(sin(3.141572*t/(S*0.5))+1)*Q(s)<<1},D=function(s){return M(s)|8&t*Math.pow(2,(1+s.charCodeAt(((S/8+t)/S)%s.length))/12-7)},2*Q("BBNNDDPPAAMMFFRR??KKDDPP==II==II")+0.5*D("hhjjdeeadca\0aaccd\0dcacehjehceacaeehhjehceadedcacddacehcdcaccaacchhjjdeeadca\0aaccd\0dcacehjehceacaeehhjehceadedcacddacehcdcacca\0a\0aa\\^aa\\^aceafefha\0aa\\^a\\feca\\YZ\\aa\\^aa\\^aacea\\^\\a\0aa\^afefhaa`aa\\^aa\\^aceafefha\0aa\\^a\\feca\\YZ\\aa\\^aa\\^aacea\\^\\a\0aa\^afefhaacc")+0.5(floor(((t+S4-100)%(S4))/(S4-100))100 | 50(floor(((t+S2)%(S4))/(S4-200))tsin(t)&0x10100) | 0.001(floor(((t+S-25)%S)/(S-25))0xf0*t))`
+// http://www.bemmu.com/music/index.html
+// Old piece of byte beat I wrote a while back - sigveseb
+
+// This was dweet #5373.  In Firefox on Windows 10 it would never stop playing, even when scrolled out of view
+s=speechSynthesis,o=new SpeechSynthesisUtterance("This easter Ninja dev is going to the demoparty Revision, join us")
+s.speaking||s.speak(o)`
+// If he adds t|| that may fix it
+
+// This one is horrid if you are wearing headphones and stumble across it: https://www.dwitter.net/d/2256
